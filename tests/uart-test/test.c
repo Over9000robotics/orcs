351ee4aa3e78e8181c797968627291c4299c363a
djordjevujic <djordjevujic@yahoo.com>
2018-01-12 20:50:40 +0000

    Testing with keyboard inputing characters, and after sending, reading receieved

--- tests/uart-test/test.c
+++ tests/uart-test/test.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
 #include <wiringPi.h> //for delay
 #include "uart.h"
+#include "color.h"
 
 int main()
 {
-	unsigned char* p_tx_buffer;
-	unsigned char* p_rx_buffer;
+	unsigned char  tx_buffer[255];
+	unsigned char* p_tx_buffer = tx_buffer;
+	
+	unsigned char  rx_buffer[255];
+	unsigned char* p_rx_buffer = rx_buffer;
 	
 	int uart0_filestream;
-	int uart0_transmit_success = 0;
-	int uart0_receive_success = 0;
+	int uart0_bytes_receieved  = 0;
 	
 	uart0_filestream = uart0_init();
 	printf("(main) uart0_filestream: %d\n", uart0_filestream);
 	
 	while(1)
 	{
-			uart0_transmit_success = uart0_transmit(uart0_filestream, 
-														p_tx_buffer);
+			int i = 0;
+			int n = 0;
+			unsigned char c;
+			printf("Type character array to send: \n");
+			
+			do
+			{
+				scanf("%c",&c);
+				tx_buffer[i++] = c;
+			}
+			while(c != 'e');
+			n = i;
+			printf("\n");
+		
+			printf("Typed array: \n");
+			print_blue();
+			for(i = 0; i < n; i++)
+			{
+				printf("%c", tx_buffer[i]);
+			}
+			print_reset();
+			printf("\n");
+			
+			uart0_transmit(uart0_filestream, p_tx_buffer, n);
 			delay (1000);	//wait 1 second
 			
-			uart0_receive_success = uart0_receive_bytes(uart0_filestream, 
-														p_rx_buffer);
+			uart0_bytes_receieved = uart0_receive_bytes(uart0_filestream, p_rx_buffer, 255);
 			delay(1000);
 	}
 	return 0;
