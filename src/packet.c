05117f09f487dbdfc26761eaeda98e4c0d33b191
djordjevujic <djordjevujic@yahoo.com>
2018-01-14 19:04:33 +0000

    packet_prepare implemented

--- src/packet.c
+++ src/packet.c
@@ -1,1 +1,96 @@
-#include <packet.h>
+/**
+ * @file packet.c
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include "packet.h"
+#include "color.h"
+#include "uart.h"
+
+static t_packet tx_packets[MAX_TX_PACKETS];
+static t_packet* tx_packet_ptr = tx_packets;
+static int8_t free_packets = 10;
+static uint8_t uart_enabled;
+
+void uart_pkt_en(uint8_t uen)
+{
+	if(uen)	/*if uen != 0*/
+	{
+		uart_enabled = 1;
+	}
+	else
+	{
+		uart_enabled = 0;
+	}
+	print_yellow();
+	printf("Uart: ");
+	
+	if(uart_enabled)
+	{
+		print_blue();
+		printf("ENABLED\n");
+	}
+	else
+	{
+		print_red();
+		printf("NOT ENABLED\n");
+	}
+	print_reset();
+}
+
+void tx_packets_init(void)
+{
+	int i = 0;
+	for (i = 0; i < MAX_TX_PACKETS; i++)
+	{
+		tx_packets[i].status = free_to_use;
+	}
+}
+
+t_packet* find_free_packet(void)
+{
+	int i = 0;
+	
+	for(i = 0; i < MAX_TX_PACKETS; i++)
+		{
+			if(tx_packets[i].status == free_to_use)
+			{
+				return &tx_packets[i];
+			}
+		}
+	return 0;
+}
+
+void packet_prepare(uint8_t type)
+{
+	if(free_packets > 0)	//Find free package
+	{
+		tx_packet_ptr = find_free_packet(); // tx_packet_ptr = &tx_packets[i]
+	
+		if(tx_packet_ptr)
+		{	
+			free_packets--;
+			tx_packet_ptr -> status = writing_packet;
+			tx_packet_ptr -> type = type;
+			tx_packet_ptr -> size = 0;
+			
+			printf("\nfree_packets: %d\n",free_packets);
+			printf("tx_packet_ptr -> status: %d\n", tx_packet_ptr -> status);	
+		}
+		else
+		{
+			print_red();
+			printf("Packet: ");
+			print_reset();
+			printf("No free packet.\n");
+			return;
+		}
+	}
+	else
+	{
+		print_red();
+		printf("Packet: NO FREE PACKET\n");
+		print_reset();
+	}
+}
--- src/packet.h
+++ src/packet.h
@@ -8,8 +8,12 @@
 #define PACKET_H_
 
 #include <stdint.h>	
+#include "packet.h"
+
 #define MAX_PKT_SIZE 32
 #define PACKET_HEADER 4
+#define MAX_TX_PACKETS 10
+#define PACKET_SYNC 0x3c
 
 enum PacketStatus {
 	free_to_use,
@@ -28,22 +32,45 @@ enum PacketStatus {
   *  L - 1 Byte payload length	\n
   *  x - L Bytes data	
   */
-typedef struct packet_t
+typedef struct t_packet
 {
 	uint8_t sync;
 	uint8_t crc;
 	uint8_t type;
 	uint8_t size;
 	uint8_t data[MAX_PKT_SIZE-PACKET_HEADER];
-	uint8_t packet_ptr;
+	uint8_t packet_byte_selector;
 	enum PacketStatus status;
-}packet_t;
+}t_packet;
+
 
 /**
- * \fn Prepares everything except message data 
+ *  Enables / disables uart using (second option would be CAN)
+ * 	and it will be implemented later.
+ *  @param uen 1 - enabled, 0 - disabled
  */
+void uart_pkt_en(uint8_t uen);
 
+
+/**
+ *  Initialises packet status to free_to_use
+ */
+void tx_packets_init(void);
+
+
+/**
+ *   Looks for free packet
+ * 	 @return free packet adress
+ */
+t_packet* find_free_packet(void);
+
+
+/**
+ * Prepares everything except message data 
+ * @param type - type of command (defined in motioncmd.h)
+ */
 void packet_prepare(uint8_t type);
+
 
 #endif
 
